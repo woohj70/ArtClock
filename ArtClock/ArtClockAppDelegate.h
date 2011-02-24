6c6b99a16b5f2761bed4e60e47d8522712a5961a
mazdah <hyoungjun.woo@gmail.com>
2011-02-24 22:32:16 +0900

    Initial Commit

---
+++ ArtClock/ArtClockAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ArtClockAppDelegate.h
+//  ArtClock
+//
+//  Created by HYOUNG JUN WOO on 11. 2. 24..
+//  Copyright 2011 Mazdah.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArtClockAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ ArtClock/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ArtClock
+//
+//  Created by HYOUNG JUN WOO on 11. 2. 24..
+//  Copyright 2011 Mazdah.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
